d5f6088994fc02c39672963df41a2f74f148b401
Raja-Dey <rddey80@gmail.com>
2015-03-08 14:44:23 -0400

    run this code to get plot3.png in your working directory

---
+++ module4-Explo-Data/plot3.R
@@ -0,0 +1,19 @@
+## Plot3
+
+data <- read.csv("unzip("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip")", 
+                 header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+subdata <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+datetime <- paste(as.Date(subdata$Date), subdata$Time)
+subdata$Datetime <- as.POSIXct(datetime)
+par(mfrow=c(1,1), mar=c(4,4,4,2), oma=c(1,1,1,1))
+with(subdata, {
+  plot(Sub_metering_1~Datetime, type="l",
+       ylab="Energy sub metering", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
