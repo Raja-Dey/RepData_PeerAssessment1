dead413f578465f1eac069b9ad4ae22942139b49
Raja-Dey <rddey80@gmail.com>
2015-03-08 14:29:12 -0400

    run this code to get plot1.png in your working directory

---
+++ module4-Explo-Data/plot1.R
@@ -0,0 +1,13 @@
+## Plot1
+
+data <- read.csv("unzip("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip")", 
+                 header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+subdata <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+datetime <- paste(as.Date(subdata$Date), subdata$Time)
+subdata$Datetime <- as.POSIXct(datetime)
+par(mfrow=c(1,1), mar=c(4,4,4,2), oma=c(1,1,1,1))
+hist(subdata$Global_active_power, main="Global Active Power",
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
